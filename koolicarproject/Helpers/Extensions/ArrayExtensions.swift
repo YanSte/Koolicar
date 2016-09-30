ea951125bdae30a3bddaedf92f997f1a76394005
Stephan Yannick <stephan.yannick@me.com>
2016-09-30 16:44:51 +0200

    Add some protocol and extensions

---
+++ koolicarproject/Helpers/Extensions/ArrayExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  ArrayExtensions.swift
+//  koolicarproject
+//
+//  Created by Stephan Yannick on 30/09/2016.
+//  Copyright © 2016 koolicar. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    public func get(_ index: Int) -> Element? {
+        return index >= 0 && index < count ? self[index] : nil
+    }
+}
---
+++ koolicarproject/Models/Protocol/ReusableIdentifierCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReusableIdentifierCell.swift
+//  koolicarproject
+//
+//  Created by Stephan Yannick on 30/09/2016.
+//  Copyright © 2016 koolicar. All rights reserved.
+//
+
+import Foundation
+
+/// Protocol Identifier for cells
+public protocol ReusableIdentifierCell {
+    static var reusableIdentifier:String { get }
+}
+
